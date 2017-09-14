694504db44f4cd927aacc46e3a35d3880c531430
Wang Siyuan <siyuan.arc@gmail.com>
2017-09-14 16:47:55 +0800

    array-based queue  and linked queue implementation

---
+++ source/data_structure/array_queue.go
@@ -0,0 +1,48 @@
+package data_structure
+
+type ArrayQueue struct {
+	size int
+	front int
+	rear int
+	listArray []Elem
+}
+
+func (a ArrayQueue) setup(size int) {
+	a.size = size
+	a.front = 0
+	a.rear = 0
+	a.listArray = make([]Elem, size)
+}
+
+func (a ArrayQueue) clear() {
+	a.front = 0
+	a.rear = 0
+}
+
+func (a ArrayQueue) enqueue(item Elem) error {
+	if a.rear + 1 % a.size == a.front {
+		return FullListError{}
+	}
+	a.rear = (a.rear + 1) % a.size
+	a.listArray[a.rear] = item
+	return nil
+}
+
+func (a ArrayQueue) dequeue() (Elem, error) {
+	if a.isEmpty() {
+		return nil, EmptyListError{}
+	}
+	a.front = (a.front + 1) % a.size
+	return a.listArray[a.front], nil
+}
+
+func (a ArrayQueue) firstValue() (Elem, error) {
+	if a.isEmpty() {
+		return nil, EmptyListError{}
+	}
+	return a.listArray[(a.front + 1) % a.size], nil
+}
+
+func (a ArrayQueue) isEmpty() bool {
+	return a.front == a.rear
+}
---
+++ source/data_structure/linked_queue.go
@@ -0,0 +1,50 @@
+package data_structure
+
+type LinkedQueue struct {
+	front *LinkNode
+	rear *LinkNode
+}
+
+func (l LinkedQueue) setup() {
+	l.front = nil
+	l.rear = nil
+}
+
+func (l LinkedQueue) clear() {
+	l.setup()
+}
+
+func (l LinkedQueue) enqueue(item Elem) {
+	if l.isEmpty() {
+		l.rear = &LinkNode{item, l.rear, nil}
+		l.front = l.rear
+	} else {
+		l.rear.next = &LinkNode{item, l.rear, nil}
+		l.rear = l.rear.next	
+	}
+}
+
+func (l LinkedQueue) dequeue() (Elem, error) {
+	if l.isEmpty() {
+		return nil, EmptyListError{}
+	}
+	value := l.front.value
+	l.front = l.front.next
+	if l.front == nil {
+		l.rear = nil
+	} else {
+		l.front.prev = nil	
+	}
+	return value, nil
+}
+
+func (l LinkedQueue) firstValue() (Elem, error) {
+	if l.isEmpty() {
+		return nil, EmptyListError{}
+	}
+	return l.front.value, nil
+}
+
+func (l LinkedQueue) isEmpty() bool {
+	return l.front == l.rear
+}
