b27b545a8409500d3c3a6dbfd1baffeadc077321
Thrimbda <siyuan.arc@gmail.com>
2017-10-15 13:54:02 +0800

    bubble and selection and test cases

--- source/algorithm/sort/sort.go
+++ source/algorithm/sort/sort.go
@@ -8,10 +8,44 @@ type Interface interface {
 	Swap(i, j int)
 }
 
-func insertionSort(array Interface) {
-	for i := 1; i < array.Len(); i++ {
-		for j := i; j > 0 && array.Less(j, j-1); j-- {
-			array.Swap(j, j-1)
+func insertionSort(data Interface) {
+	for i := 1; i < data.Len(); i++ {
+		for j := i; j > 0 && data.Less(j, j-1); j-- {
+			data.Swap(j, j-1)
 		}
 	}
 }
+
+func bubbleSort(data Interface) {
+	for i := 0; i < data.Len()-1; i++ {
+		for j := data.Len() - 1; j > i; j-- {
+			if data.Less(j, j-1) {
+				data.Swap(j, j-1)
+			}
+		}
+	}
+}
+
+func selectionSort(data Interface) {
+	for i := 0; i < data.Len()-1; i++ {
+		minIndex := i
+		for j := data.Len() - 1; j > i; j-- {
+			if data.Less(j, minIndex) {
+				minIndex = j
+			}
+		}
+		data.Swap(i, minIndex)
+	}
+}
+
+//IsSorted is a function to check whether a sequence is sorted.
+func IsSorted(data Interface) bool {
+	for i := 0; i < data.Len()-1; i++ {
+		if data.Less(i+1, i) {
+			return false
+		}
+	}
+	return true
+}
+
+//TODO: bubble sort, selection sort, quick sork, shell sork, merge sort, heap sort, bin sort, radix sort
--- source/algorithm/sort/sort_test.go
+++ source/algorithm/sort/sort_test.go
@@ -1,7 +1,6 @@
 package sort
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -21,20 +20,72 @@ func (a nums) Swap(i, j int) {
 
 func Test_insertionSort(t *testing.T) {
 	type args struct {
-		array nums
+		data nums
 	}
 	tests := []struct {
 		name string
 		args args
 	}{
 		{"sorted array", args{[]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}}},
-		{"anti sorted array", args{[]int{9, 8, 7, 6, 5, 4, 3, 2, 1}}},
+		{"anti sorted array", args{[]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}}},
 		{"random array", args{[]int{6, 2, 8, 3, 9, 7, 6, 1, 5, 0}}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			insertionSort(tt.args.array)
-			fmt.Println(tt.args.array)
+			data := tt.args.data
+			insertionSort(data)
+			if !IsSorted(data) {
+				t.Errorf("sorted %v", tt.args.data)
+				t.Errorf("   got %v", data)
+			}
+		})
+	}
+}
+
+func Test_bubbleSort(t *testing.T) {
+	type args struct {
+		data nums
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{"sorted array", args{[]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}}},
+		{"anti sorted array", args{[]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}}},
+		{"random array", args{[]int{6, 2, 8, 3, 9, 7, 6, 1, 5, 0}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			data := tt.args.data
+			bubbleSort(data)
+			if !IsSorted(data) {
+				t.Errorf("sorted %v", tt.args.data)
+				t.Errorf("   got %v", data)
+			}
+		})
+	}
+}
+
+func Test_selectionSort(t *testing.T) {
+	type args struct {
+		data nums
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{"sorted array", args{[]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}}},
+		{"anti sorted array", args{[]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}}},
+		{"random array", args{[]int{6, 2, 8, 3, 9, 7, 6, 1, 5, 0}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			data := tt.args.data
+			selectionSort(data)
+			if !IsSorted(data) {
+				t.Errorf("sorted %v", tt.args.data)
+				t.Errorf("   got %v", data)
+			}
 		})
 	}
 }
