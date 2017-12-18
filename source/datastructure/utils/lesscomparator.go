d30cf12a8340d100095799c7be2a7a4294f12054
Wang Siyuan <siyuan.arc@gmail.com>
2017-12-18 15:17:17 +0800

    A great universal less comparator

---
+++ source/datastructure/utils/lesscomparator.go
@@ -0,0 +1,155 @@
+package utils
+
+import (
+	. "../../datastructure"
+	"strings"
+)
+
+// // inspired by go std package sort, a comparator only return true if i < j.
+
+/*
+string
+
+int  int8  int16  int32  int64
+uint uint8 uint16 uint32 uint64 uintptr
+
+byte // alias for uint8
+
+rune // alias for int32
+     // represents a Unicode code point
+
+float32 float64
+
+*/
+
+func LessComparator(i, j interface{}) bool {
+	switch i.(type) {
+	case string:
+		return StringsLessComparator(i, j)
+	case int:
+		return IntsLessComparator(i, j)
+	case int8:
+		return Int8sLessComparator(i, j)
+	case int16:
+		return Int16sLessComparator(i, j)
+	case int32:
+		return Int32sLessComparator(i, j)
+	case int64:
+		return Int64sLessComparator(i, j)
+	case uint:
+		return UintsLessComparator(i, j)
+	case uint8:
+		return Uint8sLessComparator(i, j)
+	case uint16:
+		return Uint16sLessComparator(i, j)
+	case uint32:
+		return Uint32sLessComparator(i, j)
+	case uint64:
+		return Uint64sLessComparator(i, j)
+	case float32:
+		return Float32sLessComparator(i, j)
+	case float64:
+		return Float64sLessComparator(i, j)
+	default:
+		a := i.(Elem)
+		b := j.(Elem)
+		return a.LessComparator(b)
+	}
+}
+
+func StringsLessComparator(i, j interface{}) bool {
+	a := i.(string)
+	b := j.(string)
+
+	if strings.Compare(a, b) > 0 {
+		return false
+	}
+	return true
+}
+
+// TODO: except string, numerical values should be comparable.
+//       e.g. comparing 1 < 1.1 shouldn't trigger a panic but return true. 
+
+func IntsLessComparator(i, j interface{}) bool {
+	a := i.(int)
+	b := j.(int)
+
+	return a < b
+}
+
+func Int8sLessComparator(i, j interface{}) bool {
+	a := i.(int8)
+	b := j.(int8)
+
+	return a < b
+}
+
+func Int16sLessComparator(i, j interface{}) bool {
+	a := i.(int16)
+	b := j.(int16)
+
+	return a < b
+}
+
+func Int32sLessComparator(i, j interface{}) bool {
+	a := i.(int32)
+	b := j.(int32)
+
+	return a < b
+}
+
+func Int64sLessComparator(i, j interface{}) bool {
+	a := i.(int64)
+	b := j.(int64)
+
+	return a < b
+}
+
+func UintsLessComparator(i, j interface{}) bool {
+	a := i.(uint)
+	b := j.(uint)
+
+	return a < b
+}
+
+func Uint8sLessComparator(i, j interface{}) bool {
+	a := i.(uint8)
+	b := j.(uint8)
+
+	return a < b
+}
+
+func Uint16sLessComparator(i, j interface{}) bool {
+	a := i.(uint16)
+	b := j.(uint16)
+
+	return a < b
+}
+
+func Uint32sLessComparator(i, j interface{}) bool {
+	a := i.(uint32)
+	b := j.(uint32)
+
+	return a < b
+}
+
+func Uint64sLessComparator(i, j interface{}) bool {
+	a := i.(uint64)
+	b := j.(uint64)
+
+	return a < b
+}
+
+func Float32sLessComparator(i, j interface{}) bool {
+	a := i.(float32)
+	b := j.(float32)
+
+	return a < b
+}
+
+func Float64sLessComparator(i, j interface{}) bool {
+	a := i.(float64)
+	b := j.(float64)
+
+	return a < b
+}
---
+++ source/datastructure/utils/lesscomparator_test.go
@@ -0,0 +1,86 @@
+package utils
+
+import (
+	. "../../datastructure"
+	"testing"
+)
+
+func TestStringsLessComparator(t *testing.T) {
+	data := [][]interface{}{
+		{"a", "b", true},
+		{"a", "A", false},
+		{"0", "a", true},
+		{" ", "9", true},
+		{"asdasd", "v", true},
+	}
+	for i, testCase := range data {
+		got := StringsLessComparator(testCase[0], testCase[1])
+		expect := testCase[2]
+		if got != expect {
+			t.Errorf("in test case %v, expect %v, but got %v", i, expect, got)
+		}
+	}
+}
+
+func TestIntsLessComparator(t *testing.T) {
+	data := [][]interface{}{
+		{1, 2, true},
+		{0, 1, true},
+		{-1, 1, true},
+		{0, 0, false},
+		{12345, 54321, true},
+	}
+	for i, testCase := range data {
+		got := IntsLessComparator(testCase[0], testCase[1])
+		expect := testCase[2]
+		if got != expect {
+			t.Errorf("in test case %v, expect %v, but got %v", i, expect, got)
+		}
+	}
+}
+
+func TestFloat64sLessComparator(t *testing.T) {
+	data := [][]interface{}{
+		{1.1, 2.2, true},
+		{0.0, 1.0, true},
+		{-1.1, 1.2, true},
+		{0.0, 0.0, false},
+		{1.123123, 1.1231234, true},
+	}	
+	for i, testCase := range data {
+		got := Float64sLessComparator(testCase[0], testCase[1])
+		expect := testCase[2]
+		if got != expect {
+			t.Errorf("in test case %v, expect %v, but got %v", i, expect, got)
+		}
+	}
+}
+
+type City struct {
+	id int
+}
+func (c City) LessComparator(b Elem) bool {
+	return c.id < b.(*City).id
+}
+func (c City) String() string {
+	return string(c.id)
+}
+
+func TestLessComparator(t *testing.T) {
+	data := [][]interface{}{
+		{1.1, 2.2, true},
+		{"s", "A", false},
+		{&City{12}, &City{1}, false},
+		{&City{0}, &City{15}, true},
+		{&City{3}, &City{3}, false},
+		{0, 0, false},
+		{1.123123, 1.1231234, true},
+	}	
+	for i, testCase := range data {
+		got := LessComparator(testCase[0], testCase[1])
+		expect := testCase[2]
+		if got != expect {
+			t.Errorf("in test case %v, expect %v, but got %v", i, expect, got)
+		}
+	}
+}
