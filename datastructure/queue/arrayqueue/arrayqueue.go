471465c8ad9d9e0b91d34f97626f9b8a7ce6d7d9
Wang Siyuan <siyuan.arc@gmail.com>
2018-01-01 16:17:06 +0800

    array queue reform done, adapt array list.

--- datastructure/queue/arrayqueue/arrayqueue.go
+++ datastructure/queue/arrayqueue/arrayqueue.go
@@ -6,6 +6,7 @@ import (
 )
 
 type ArrayQueue struct {
+	size  int
 	front int
 	rear  int
 	list  *arraylist.ArrayList
@@ -13,36 +14,43 @@ type ArrayQueue struct {
 
 func NewArrayQueue(size int) *ArrayQueue {
 	list := arraylist.NewArrayList(size)
-	return &ArrayQueue{0, 0, list}
+	return &ArrayQueue{size, 0, 0, list}
 }
 
-func (a ArrayQueue) clear() {
-	a.list.Clear()
+func (a *ArrayQueue) clear() {
+	a.front = 0
+	a.rear = 0
 }
 
-func (a ArrayQueue) Enqueue(item interface{}) {
-	if a.rear+1%a.list.Length() == a.front {
+func (a *ArrayQueue) Enqueue(item interface{}) {
+	if a.size == 0 || a.rear+1%a.size == a.front {
 		panic(&FullListError{})
 	}
-	a.rear = (a.rear + 1) % a.list.Length()
-	a.list.SetValue(a.rear, item)
+	if a.rear+1 >= a.list.Length() {
+		a.rear = (a.rear + 1) % a.size
+		a.list.Append(item)
+	} else {
+		a.list.SetValue(a.rear, item)
+		a.rear += 1
+	}
 }
 
-func (a ArrayQueue) Dequeue() interface{} {
+func (a *ArrayQueue) Dequeue() interface{} {
 	if a.IsEmpty() {
 		panic(&EmptyListError{})
 	}
-	a.front = (a.front + 1) % a.list.Length()
+	value := a.list.Get(a.front)
+	a.front = (a.front + 1) % a.size
+	return value
+}
+
+func (a *ArrayQueue) Peek() interface{} {
+	if a.IsEmpty() {
+		panic(&EmptyListError{})
+	}
 	return a.list.Get(a.front)
 }
 
-func (a ArrayQueue) Peek() interface{} {
-	if a.IsEmpty() {
-		panic(&EmptyListError{})
-	}
-	return a.list.Get((a.front + 1) % a.list.Length())
-}
-
-func (a ArrayQueue) IsEmpty() bool {
+func (a *ArrayQueue) IsEmpty() bool {
 	return a.front == a.rear
 }
---
+++ datastructure/queue/arrayqueue/arrayqueue_test.go
@@ -0,0 +1,174 @@
+package queue
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/Thrimbda/dune/datastructure/arrayutils"
+	"github.com/Thrimbda/dune/datastructure/list/arraylist"
+)
+
+func TestNewArrayQueue(t *testing.T) {
+	type args struct {
+		size int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ArrayQueue
+	}{
+		{"new_empty", args{0}, &ArrayQueue{0, 0, 0, arraylist.NewArrayList(0)}},
+		{"new1", args{3}, &ArrayQueue{3, 0, 0, arraylist.NewArrayList(3)}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewArrayQueue(tt.args.size); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewArrayQueue() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestArrayQueue_clear(t *testing.T) {
+	tests := []struct {
+		name string
+		a    *ArrayQueue
+	}{
+		{"empty1", &ArrayQueue{3, 0, 3, arraylist.ConvertToLinkedList(3, 1, 2, 3)}},
+		{"empty2", &ArrayQueue{3, 3, 100, arraylist.ConvertToLinkedList(3, make([]int, 100, 100))}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.a.clear()
+			if tt.a.front != 0 && tt.a.rear != 0 {
+				t.Errorf("Clear failed, got front pointer %v, rear pointer %v", tt.a.front, tt.a.rear)
+			}
+		})
+	}
+}
+
+func TestArrayQueue_Enqueue(t *testing.T) {
+	type args struct {
+		item interface{}
+	}
+	tests := []struct {
+		name      string
+		a         *ArrayQueue
+		args      args
+		length    int
+		element   interface{}
+		testPanic error
+	}{
+		{"enqueue", NewArrayQueue(3), args{1}, 1, 1, nil},
+		{"panic", NewArrayQueue(0), args{2}, 1, 2, &arrayutils.FullListError{}},
+		{"panic", &ArrayQueue{2, 0, 1, arraylist.ConvertToLinkedList(2, 1, 2)}, args{2}, 1, 2, &arrayutils.FullListError{}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.testPanic == nil {
+				tt.a.Enqueue(tt.args.item)
+				if got := tt.a.Peek(); !reflect.DeepEqual(got, tt.element) {
+					t.Errorf("expect %v, but got %v", tt.element, got)
+				}
+				if got := func() int {
+					if tt.a.front > tt.a.rear {
+						return tt.a.rear + tt.a.size - tt.a.front
+					}
+					return tt.a.rear - tt.a.front
+				}(); got != tt.length {
+					t.Errorf("expect front %v, but got front %v", tt.length, got)
+				}
+			} else {
+				defer func() {
+					if r := recover(); !reflect.DeepEqual(r, tt.testPanic) {
+						t.Errorf("expect %v, but got %v", tt.testPanic, r)
+					}
+				}()
+				tt.a.Enqueue(tt.args.item)
+			}
+		})
+	}
+}
+
+func TestArrayQueue_Dequeue(t *testing.T) {
+	tests := []struct {
+		name      string
+		a         *ArrayQueue
+		length    int
+		want      interface{}
+		testPanic error
+	}{
+		{"dequeue", &ArrayQueue{2, 0, 1, arraylist.ConvertToLinkedList(2, 1, 2)}, 0, 1, nil},
+		{"panic", &ArrayQueue{2, 1, 1, arraylist.ConvertToLinkedList(2, 1, 2)}, 0, 1, &arrayutils.EmptyListError{}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.testPanic == nil {
+				if got := tt.a.Dequeue(); !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("ArrayQueue.Dequeue() = %v, want %v", got, tt.want)
+				}
+				if got := func() int {
+					if tt.a.front > tt.a.rear {
+						return tt.a.rear + tt.a.size - tt.a.front
+					}
+					return tt.a.rear - tt.a.front
+				}(); got != tt.length {
+					t.Errorf("expect front %v, but got front %v", tt.length, got)
+				}
+			} else {
+				defer func() {
+					if r := recover(); !reflect.DeepEqual(r, tt.testPanic) {
+						t.Errorf("expect %v, but got %v", tt.testPanic, r)
+					}
+				}()
+				tt.a.Dequeue()
+			}
+		})
+	}
+}
+
+func TestArrayQueue_Peek(t *testing.T) {
+	tests := []struct {
+		name      string
+		a         *ArrayQueue
+		want      interface{}
+		testPanic error
+	}{
+		{"peek", &ArrayQueue{2, 0, 1, arraylist.ConvertToLinkedList(2, 1, 2)}, 1, nil},
+		{"panic", &ArrayQueue{2, 1, 1, arraylist.ConvertToLinkedList(2, 1, 2)}, 1, &arrayutils.EmptyListError{}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.testPanic == nil {
+				if got := tt.a.Peek(); !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("ArrayQueue.Dequeue() = %v, want %v", got, tt.want)
+				}
+			} else {
+				defer func() {
+					if r := recover(); !reflect.DeepEqual(r, tt.testPanic) {
+						t.Errorf("expect %v, but got %v", tt.testPanic, r)
+					}
+				}()
+				tt.a.Peek()
+			}
+		})
+	}
+}
+
+func TestArrayQueue_IsEmpty(t *testing.T) {
+	tests := []struct {
+		name string
+		a    *ArrayQueue
+		want bool
+	}{
+		{"empty", &ArrayQueue{2, 0, 1, arraylist.ConvertToLinkedList(2, 1, 2)}, false},
+		{"not_empty", &ArrayQueue{2, 1, 1, arraylist.ConvertToLinkedList(2, 1, 2)}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.a.IsEmpty(); got != tt.want {
+				t.Errorf("ArrayQueue.IsEmpty() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
